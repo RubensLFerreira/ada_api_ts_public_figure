de471180412f8dd47b644f3ecbd4d74d47769a02
RubensLFerreira <rubenslima000@gmail.com>
2024-01-27 11:42:08 -0300

    feat: Route find all created

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,11 +2,11 @@ import { Router } from 'express';
 
 import authenticated from '../middlewares/authenticated';
 
-import createUser from '../controllers/User/Create';
+import { createUser, getAllUsers } from '../controllers/User';
 
 const router = Router();
 
-router.route('/').post(createUser);
+router.route('/').get(getAllUsers).post(createUser);
 router.route('/login').post(authenticated);
 
 export default router;
